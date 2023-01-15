e3771b7880624a591067076610a96e8e8e54cb2f
付志强 <zhiqiangfu6@gmail.com>
2023-01-15 17:45:17 +0800

    feat: flattable

---
+++ src/isArguments.ts
@@ -0,0 +1,23 @@
+import getTag from './.internal/getTag'
+import isObjectLike from './isObjectLike'
+
+/**
+ * Checks if `value` is likely an `arguments` object.
+ *
+ * @since 0.1.0
+ * @category Lang
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is an `arguments` object, else `false`.
+ * @example
+ *
+ * isArguments(function() { return arguments }())
+ * // => true
+ *
+ * isArguments([1, 2, 3])
+ * // => false
+ */
+function isArguments(value) {
+    return isObjectLike(value) && getTag(value) == '[object Arguments]'
+}
+
+export default isArguments
